bfa9278ead4f781769c756d9ecb2797e894e12b3
AnshSaiwal <anshsaiwal28726@gmail.com>
2021-09-30 21:00:31 +0530

    second commit

---
+++ my.java
@@ -0,0 +1,22 @@
+class my
+{
+	public static void main(String[] args)
+	{
+		for(int i=2;i<=100;i++)
+		{
+			int flag=0;
+			for(int j=2;j<=i-1;j++)
+			{
+				if(i%j==0)
+				{
+					flag=1;
+					break;
+				}
+			}
+			if(flag==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
